38cd4a49ea7973426d1b06ec41a8e4cbbd3828dd
Brian Dumbacher <brian.dumbacher@gmail.com>
2018-09-22 18:26:24 -0400

    Create s1_download.py
    Create s1_download.py

---
+++ s1_download.py
@@ -0,0 +1,15 @@
+#Name:            s1_download.py
+#Purpose:         Download PDFs that were discovered during web crawling and whose URLs are found on /project/dump/dump.csv
+#Data Layout:     See README.md
+#Python Version:  3
+
+import os
+
+def main():
+    #Project name
+    projname = "project"
+        
+    return
+
+if __name__ == "__main__":
+    main()
