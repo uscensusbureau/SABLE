973e60784e1e5c6ae9c48b461b922f84620dee3b
Brian Dumbacher <brian.dumbacher@gmail.com>
2018-09-24 09:49:10 -0400

    Update s1_download.py
    Add the functions "is_pdf" and "download_pdf" and write code in the "main" function for reading in the list of URLs

--- s1_download.py
+++ s1_download.py
@@ -3,13 +3,41 @@
 #Data Layout:     See README.md
 #Python Version:  3
 
+import codecs
 import csv
 import os
+import re
+
+#Name:       is_pdf
+#Arguments:  url
+#            metadata
+#Purpose:    Determine whether the URL points to a PDF
+
+def is_pdf(url, metadata):
+    urlmatch = re.search(r"^(\S+)\.([pP][dD][fF])$", url)
+    metadatamatch = re.search(r"Content-Type:application/pdf", metadata)
+    return urlmatch or metadatamatch
+
+#Name:       download_pdf
+#Arguments:  url
+#Purpose:    Download the PDF
+
+def download_pdf(url):
+    os.system("wget --no-check-certificate -nv -P /project/download/ " + url)
+    return
 
 def main():
     #Project name
     projname = "project"
-        
+    
+    #Read in list of URLs and download PDFs
+    f = codecs.open("/project/dump/dump.csv", "rU")
+    rdr = csv.DictReader(f)
+    for row in rdr:
+        if is_pdf(row["Url"], row["Metadata"]):
+            download_pdf(row["Url"], project)
+    f.close()
+    
     return
 
 if __name__ == "__main__":
