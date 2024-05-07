d2b9fe5f4f1bb769e4226540adf16b8ab6c77844
Brian Dumbacher <brian.dumbacher@gmail.com>
2024-05-07 14:37:56 -0400

    Update m0_setup.py
    Use string format method

--- m0_setup.py
+++ m0_setup.py
@@ -9,7 +9,7 @@ import sys
 #Name:        valid_arguments
 #Purpose:     Check whether the command-line arguments are valid
 #Parameters:  sys.argv (globally defined list of command-line arguments)
-#Returns:     True (arguments are valid) or False (arguments are invalid)
+#Returns:     True (all arguments are valid) or False (at least one argument is invalid)
 
 def valid_arguments():
     if len(sys.argv) == 2 and re.search(r"^[a-zA-Z][a-zA-Z_-]*$", sys.argv[1]):
@@ -22,16 +22,16 @@ def valid_arguments():
 #Returns:     
 
 def create_folders(projName):
-    projDir = "/" + projName + "/"
+    projDir = "/{}/".format(projName)
     if os.path.isdir(projDir):
-        print("\nProject folder " + projDir + " already exists\n")
+        print("\nProject folder {} already exists\n".format(projDir))
     else:
-        os.system("mkdir " + projDir)
-        os.system("mkdir " + projDir + "dat/")
-        os.system("mkdir " + projDir + "pdf/")
-        os.system("mkdir " + projDir + "prod/")
-        os.system("mkdir " + projDir + "txt/")
-        print("\nProject folder " + projDir + " and subfolders created\n")
+        os.system("mkdir {}".format(projDir))
+        os.system("mkdir {}dat/".format(projDir))
+        os.system("mkdir {}pdf/".format(projDir))
+        os.system("mkdir {}prod/".format(projDir))
+        os.system("mkdir {}txt/".format(projDir))
+        print("\nProject folder {} and subfolders created\n".format(projDir))
     return
 
 def main():
