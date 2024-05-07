e07414b745262d31f66f32d3c2d3afa9252c6b83
Brian Dumbacher <brian.dumbacher@gmail.com>
2024-05-07 14:43:55 -0400

    Update s0_setup.py
    Use string format method

--- s0_setup.py
+++ s0_setup.py
@@ -9,7 +9,7 @@ import sys
 #Name:        valid_arguments
 #Purpose:     Check whether the command-line arguments are valid
 #Parameters:  sys.argv (globally defined list of command-line arguments)
-#Returns:     True (arguments are valid) or False (arguments are invalid)
+#Returns:     True (all arguments are valid) or False (at least one argument is invalid)
 
 def valid_arguments():
     if len(sys.argv) == 2 and re.search(r"^[a-zA-Z][a-zA-Z_-]*$", sys.argv[1]):
@@ -22,28 +22,28 @@ def valid_arguments():
 #Returns:     
 
 def create_folders(projName):
-    projDir = "/" + projName + "/"
+    projDir = "/{}/".format(projName)
     if os.path.isdir(projDir):
-        print("\nProject folder " + projDir + " already exists\n")
+        print("\nProject folder {} already exists\n".format(projDir))
     else:
-        os.system("mkdir " + projDir)
-        os.system("mkdir " + projDir + "crawl/")
-        os.system("mkdir " + projDir + "download/")
+        os.system("mkdir {}".format(projDir))
+        os.system("mkdir {}crawl/".format(projDir))
+        os.system("mkdir {}download/".format(projDir))
         #The folder /projName/dump/ is created by Apache Nutch when its database contents are output to CSV format
-        os.system("mkdir " + projDir + "neg_pdf/")
-        os.system("mkdir " + projDir + "neg_prob/")
-        os.system("mkdir " + projDir + "neg_txt/")
-        os.system("mkdir " + projDir + "neg_xml/")
-        os.system("mkdir " + projDir + "pos_pdf/")
-        os.system("mkdir " + projDir + "pos_prob/")
-        os.system("mkdir " + projDir + "pos_txt/")
-        os.system("mkdir " + projDir + "pos_xml/")
-        os.system("mkdir " + projDir + "pred_pdf/")
-        os.system("mkdir " + projDir + "pred_prob/")
-        os.system("mkdir " + projDir + "pred_txt/")
-        os.system("mkdir " + projDir + "pred_xml/")
-        os.system("mkdir " + projDir + "urls/")
-        print("\nProject folder " + projDir + " and subfolders created\n")
+        os.system("mkdir {}neg_pdf/".format(projDir))
+        os.system("mkdir {}neg_prob/".format(projDir))
+        os.system("mkdir {}neg_txt/".format(projDir))
+        os.system("mkdir {}neg_xml/".format(projDir))
+        os.system("mkdir {}pos_pdf/".format(projDir))
+        os.system("mkdir {}pos_prob/".format(projDir))
+        os.system("mkdir {}pos_txt/".format(projDir))
+        os.system("mkdir {}pos_xml/".format(projDir))
+        os.system("mkdir {}pred_pdf/".format(projDir))
+        os.system("mkdir {}pred_prob/".format(projDir))
+        os.system("mkdir {}pred_txt/".format(projDir))
+        os.system("mkdir {}pred_xml/".format(projDir))
+        os.system("mkdir {}urls/".format(projDir))
+        print("\nProject folder {} and subfolders created\n".format(projDir))
     return
 
 def main():
