d000049327685f7e3ca156976d140903a739f8a2
Brian Dumbacher <brian.dumbacher@census.gov>
2025-03-08 15:36:07 -0500

    Update s4_logistic.py
    Edited comments

--- s4_logistic.py
+++ s4_logistic.py
@@ -1,6 +1,6 @@
-#Name:        s4_logistic.py
-#Purpose:     Classify new PDFs as positive or negative using a logistic regression model and output predicted classes and probabilities
-#Invocation:  python3 s4_logistic.py <projName>
+# Name:        s4_logistic.py
+# Purpose:     Classify new PDFs as positive or negative using a logistic regression model and output predicted classes and probabilities
+# Invocation:  python3 s4_logistic.py <projName>
 
 import codecs
 from nltk.classify import *
@@ -18,54 +18,54 @@ from sklearn.metrics import *
 from sklearn.model_selection import *
 import sys
 
-#Name:        valid_arguments
-#Purpose:     Check whether the command-line arguments are valid
-#Parameters:  sys.argv (globally defined list of command-line arguments)
-#Returns:     True (all arguments are valid) or False (at least one argument is invalid)
+# Name:        valid_arguments
+# Purpose:     Check whether the command-line arguments are valid
+# Parameters:  sys.argv (globally defined list of command-line arguments)
+# Returns:     True (all arguments are valid) or False (at least one argument is invalid)
 
 def valid_arguments():
     if len(sys.argv) == 2 and re.search(r"^[a-zA-Z][a-zA-Z_-]*$", sys.argv[1]):
         return True
     return False
 
-#Name:        get_feats_inds
-#Purpose:     Create model features, which are binary indicators of 1-grams and 2-grams
-#Parameters:  text (string of text)
-#Returns:     Dictionary of binary indicators of 1-grams and 2-grams in text
+# Name:        get_feats_inds
+# Purpose:     Create model features, which are binary indicators of 1-grams and 2-grams
+# Parameters:  text (string of text)
+# Returns:     Dictionary of binary indicators of 1-grams and 2-grams in text
 
 def get_feats_inds(text):
     t = Text(word_tokenize(text))
     g1s = [(g, True) for g in ngrams(t, 1)]
     g2s = [(g, True) for g in ngrams(t, 2)]
-    #3-grams, 4-grams, and so on can also be used
-    #g3s = [(g, True) for g in ngrams(t, 3)]
+    # 3-grams, 4-grams, and so on can also be used
+    # g3s = [(g, True) for g in ngrams(t, 3)]
     return dict(g1s + g2s)
 
-#Name:        get_feats_counts
-#Purpose:     Create model features, which are counts of 1-grams and 2-grams
-#Parameters:  text (string of text)
-#Returns:     Dictionary of counts of 1-grams and 2-grams in text
+# Name:        get_feats_counts
+# Purpose:     Create model features, which are counts of 1-grams and 2-grams
+# Parameters:  text (string of text)
+# Returns:     Dictionary of counts of 1-grams and 2-grams in text
 
 def get_feats_counts(text):
     t = Text(word_tokenize(text))
     g1s = [(g, count) for g, count in FreqDist(ngrams(t, 1)).items()]
     g2s = [(g, count) for g, count in FreqDist(ngrams(t, 2)).items()]
-    #3-grams, 4-grams, and so on can also be used
-    #g3s = [(g, count) for g, count in FreqDist(ngrams(t, 3)).items()]
+    # 3-grams, 4-grams, and so on can also be used
+    # g3s = [(g, count) for g, count in FreqDist(ngrams(t, 3)).items()]
     return dict(g1s + g2s)
 
-#Name:        format_prob
-#Purpose:     Round probability to four decimal places and convert it into a string
-#Parameters:  prob (probability)
-#Returns:     String representing probability rounded to four decimal places
+# Name:        format_prob
+# Purpose:     Round probability to four decimal places and convert it into a string
+# Parameters:  prob (probability)
+# Returns:     String representing probability rounded to four decimal places
 
 def format_prob(prob):
     return str(round(prob, 4))
 
-#Name:        fit_and_predict
-#Purpose:     Fit a logistic regression model and output predicted classes and probabilities
-#Parameters:  projName (project name)
-#Returns:     
+# Name:        fit_and_predict
+# Purpose:     Fit a logistic regression model and output predicted classes and probabilities
+# Parameters:  projName (project name)
+# Returns:     
 
 def fit_and_predict(projName):
     posTexts  = []
@@ -75,7 +75,7 @@ def fit_and_predict(projName):
     predTexts = []
     predDocs  = []
     
-    #Read in text from documents classified as positive
+    # Read in text from documents classified as positive
     posDir = sorted(os.listdir("/{}/pos_txt/".format(projName)))
     for f in posDir:
         nameMatch = re.search(r"^(\S+)\.txt$", f)
@@ -86,7 +86,7 @@ def fit_and_predict(projName):
             posTexts.append(tmpFile.readlines()[0])
             tmpFile.close()
     
-    #Read in text from documents classified as negative
+    # Read in text from documents classified as negative
     negDir = sorted(os.listdir("/{}/neg_txt/".format(projName)))
     for f in negDir:
         nameMatch = re.search(r"^(\S+)\.txt$", f)
@@ -97,12 +97,12 @@ def fit_and_predict(projName):
             negTexts.append(tmpFile.readlines()[0])
             tmpFile.close()
     
-    #Create features based on n-gram indicators
+    # Create features based on n-gram indicators
     posFeatsTrain = [(get_feats_inds(posText), "pos") for posText in posTexts]
     negFeatsTrain = [(get_feats_inds(negText), "neg") for negText in negTexts]
     featsTrain = posFeatsTrain + negFeatsTrain
     
-    #Read in text from documents for prediction
+    # Read in text from documents for prediction
     predDir = sorted(os.listdir("/{}/pred_txt/".format(projName)))
     for f in predDir:
         nameMatch = re.search(r"^(\S+)\.txt$", f)
@@ -113,7 +113,7 @@ def fit_and_predict(projName):
             predTexts.append(tmpFile.readlines()[0])
             tmpFile.close()
     
-    #Print number of positive and negative observations used for training and number of observations for prediction
+    # Print number of positive and negative observations used for training and number of observations for prediction
     print("")
     print("Positive Training: {}".format(len(posTexts)))
     print("Negative Training: {}".format(len(negTexts)))
@@ -121,7 +121,7 @@ def fit_and_predict(projName):
     
     if len(posFeatsTrain) >= 10 and len(negFeatsTrain) >= 10:
     
-        #Fit a logistic regression model and apply it to new observations
+        # Fit a logistic regression model and apply it to new observations
         print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
         print("@@@   Logistic Regression   @@@")
         print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
@@ -130,7 +130,7 @@ def fit_and_predict(projName):
         predClasses = [classifierLR.classify(get_feats_inds(predText)) for predText in predTexts]
         predProbs = [classifierLR.prob_classify(get_feats_inds(predText)) for predText in predTexts]
         
-        #Print predicted positives
+        # Print predicted positives
         print("Predicted Positives")
         print("-------------------")
         for i in range(len(predTexts)):
@@ -138,7 +138,7 @@ def fit_and_predict(projName):
                 print(predDocs[i])
         print("")
         
-        #Create output
+        # Create output
         outputFile = "/{}/pred_output.txt".format(projName)
         varNames = ["docName", "predClass", "probPos", "probNeg"]
         f = open(outputFile, "w")
