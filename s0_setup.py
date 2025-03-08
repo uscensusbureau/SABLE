ca2a76b6cbef42ad0f59fac89d75d8e7c2863b2e
Brian Dumbacher <brian.dumbacher@census.gov>
2025-03-08 15:29:14 -0500

    Update s0_setup.py
    Edited comments

--- s0_setup.py
+++ s0_setup.py
@@ -1,25 +1,25 @@
-#Name:        s0_setup.py
-#Purpose:     Set up project folders
-#Invocation:  python3 s0_setup.py <projName>
+# Name:        s0_setup.py
+# Purpose:     Set up project folders
+# Invocation:  python3 s0_setup.py <projName>
 
 import os
 import re
 import sys
 
-#Name:        valid_arguments
-#Purpose:     Check whether the command-line arguments are valid
-#Parameters:  sys.argv (globally defined list of command-line arguments)
-#Returns:     True (all arguments are valid) or False (at least one argument is invalid)
+# Name:        valid_arguments
+# Purpose:     Check whether the command-line arguments are valid
+# Parameters:  sys.argv (globally defined list of command-line arguments)
+# Returns:     True (all arguments are valid) or False (at least one argument is invalid)
 
 def valid_arguments():
     if len(sys.argv) == 2 and re.search(r"^[a-zA-Z][a-zA-Z_-]*$", sys.argv[1]):
         return True
     return False
 
-#Name:        create_folders
-#Purpose:     Create project folders
-#Parameters:  projName (project name)
-#Returns:     
+# Name:        create_folders
+# Purpose:     Create project folders
+# Parameters:  projName (project name)
+# Returns:     
 
 def create_folders(projName):
     projDir = "/{}/".format(projName)
@@ -29,7 +29,7 @@ def create_folders(projName):
         os.system("mkdir {}".format(projDir))
         os.system("mkdir {}crawl/".format(projDir))
         os.system("mkdir {}download/".format(projDir))
-        #The folder /projName/dump/ is created by Apache Nutch when its database contents are output to CSV format
+        # The folder /projName/dump/ is created by Apache Nutch when its database contents are output to CSV format
         os.system("mkdir {}neg_pdf/".format(projDir))
         os.system("mkdir {}neg_prob/".format(projDir))
         os.system("mkdir {}neg_txt/".format(projDir))
